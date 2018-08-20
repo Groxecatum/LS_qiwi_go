5f5f6ab6caf73b8c1b5bdc194d378150b97f3fa2
ysklyarov <ysklyarov@kopilka.kz>
2018-08-20 11:16:00 +0600

    + first commit

--- common.go
+++ common.go
@@ -1,1 +1,20 @@
 package golang_commons
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func ParseReqByte(r *http.Request) ([]byte, error) {
+	b, err := ioutil.ReadAll(r.Body)
+	return b, err
+}
+
+func FailOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
