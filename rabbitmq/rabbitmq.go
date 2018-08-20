b8fdf82d8d5dcaf094afda5dc899f989abf115e8
ysklyarov <ysklyarov@kopilka.kz>
2018-08-20 15:00:51 +0600

    + fix

--- rabbitmq/rabbitmq.go
+++ rabbitmq/rabbitmq.go
@@ -1,30 +1,30 @@
-package golang_commons
+package rabbitmq
 
 import (
 	"fmt"
 	"github.com/streadway/amqp"
-	"golang_commons/common"
+	"gogs.kopilka.kz/backend/golang_commons"
 )
 
 // FailOnError обоснован? или нет? с одной стороны - не может достучаться до рэббита - значит, не работает
 func Publish(queue string, b []byte, rabbitMQUser, rabbitMQPassword, rabbitMQUrl string) {
 	addr := fmt.Sprint("amqp://", rabbitMQUser, ":", rabbitMQPassword, "@", rabbitMQUrl)
 	conn, err := amqp.Dial(addr)
-	common.FailOnError(err, "Failed to connect to RabbitMQ")
+	golang_commons.FailOnError(err, "Failed to connect to RabbitMQ")
 
 	defer conn.Close()
 
 	ch, err := conn.Channel()
-	common.FailOnError(err, "Failed to open a channel")
+	golang_commons.FailOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(queue, true, false, false, false, nil)
-	common.FailOnError(err, "Failed to declare a queue")
+	golang_commons.FailOnError(err, "Failed to declare a queue")
 
 	err = ch.Publish("", q.Name, false, false,
 		amqp.Publishing{
 			ContentType: "application/json",
 			Body:        b,
 		})
-	common.FailOnError(err, "Failed to publish a message")
+	golang_commons.FailOnError(err, "Failed to publish a message")
 }
