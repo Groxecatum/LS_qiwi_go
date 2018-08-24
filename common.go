1ee75a5142be040a350c89ba8ae7146ac2debca5
ysklyarov <ysklyarov@kopilka.kz>
2018-08-24 11:27:53 +0600

    + invert

--- common.go
+++ common.go
@@ -18,3 +18,10 @@ func FailOnError(err error, msg string) {
 		panic(fmt.Sprintf("%s: %s", msg, err))
 	}
 }
+
+func Invert(b []byte) []byte {
+	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
+		b[i], b[j] = b[j], b[i]
+	}
+	return b
+}
