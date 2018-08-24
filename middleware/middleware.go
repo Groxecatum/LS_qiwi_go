25fe8af42faad4bf4b8c08e98a3fe6e16909cac8
ysklyarov <ysklyarov@kopilka.kz>
2018-08-24 08:16:26 +0600

    + middleware

---
+++ middleware/middleware.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+func Chain(f http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
+	for _, m := range middlewares {
+		f = m(f)
+	}
+	return f
+}
+
+func Method(m string) Middleware {
+	return func(f http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			if r.Method != m {
+				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+				return
+			}
+			f(w, r)
+		}
+	}
+}
+
+func Logging() Middleware {
+	return func(f http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			defer func() { log.Println(r.URL.Path, time.Since(start)) }()
+			f(w, r)
+		}
+	}
+}
