6d9f8bae229f6a563ae98051ff1b8c5cb0612d65
ysklyarov <ysklyarov@kopilka.kz>
2018-09-21 09:55:35 +0600

    + листенер для рэббита

--- rabbitmq.go
+++ rabbitmq.go
@@ -3,7 +3,10 @@ package golang_commons
 import (
 	"fmt"
 	"github.com/streadway/amqp"
+	"log"
 )
+
+type RMQHandlerFunc func(delivery amqp.Delivery)
 
 // FailOnError обоснован? или нет? с одной стороны - не может достучаться до рэббита - значит, не работает
 func Publish(queue string, b []byte, rabbitMQUser, rabbitMQPassword, rabbitMQUrl string) {
@@ -26,4 +29,41 @@ func Publish(queue string, b []byte, rabbitMQUser, rabbitMQPassword, rabbitMQUrl
 			Body:        b,
 		})
 	FailOnError(err, "Failed to publish a message")
+}
+
+func ListenAndRecieve(queue string, handler RMQHandlerFunc, rabbitMQUser, rabbitMQPassword, rabbitMQUrl string) {
+	conn, err := amqp.Dial("amqp://" + rabbitMQUser + ":" + rabbitMQPassword + " + @" + rabbitMQUrl + "/")
+	FailOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	FailOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		queue, // name
+		true,  // durable
+		false, // delete when usused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	FailOnError(err, "Failed to declare a queue")
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		false,  // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	FailOnError(err, "Failed to register a consumer")
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+			handler(d)
+		}
+	}()
 }
